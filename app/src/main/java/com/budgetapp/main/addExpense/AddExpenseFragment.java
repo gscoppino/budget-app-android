39530cbdef665136510e023d5ffeed93a8b46363
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2019-09-15 16:52:10 -0400

    Cleaned up AddExpenseFragment class implementation.

--- app/src/main/java/com/budgetapp/main/addExpense/AddExpenseFragment.java
+++ app/src/main/java/com/budgetapp/main/addExpense/AddExpenseFragment.java
@@ -29,21 +29,28 @@ import retrofit2.Response;
  * create an instance of this fragment.
  */
 public class AddExpenseFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
     public static final String USER_ID_KEY = "USER_ID";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private int userId;
 
     private EditText expenseAmountWidget;
 
     private OnFragmentInteractionListener mListener;
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson
+     * <a href="http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     * </p>
+     */
+    public interface OnFragmentInteractionListener {
+        void onSubmitExpense(final View view);
+    }
 
     public AddExpenseFragment() {
         // Required empty public constructor
@@ -53,17 +60,12 @@ public class AddExpenseFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @param userId UserID
+     * @param userId The user id.
      * @return A new instance of fragment AddExpenseFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static AddExpenseFragment newInstance(String param1, String param2, int userId) {
+    public static AddExpenseFragment newInstance(int userId) {
         AddExpenseFragment fragment = new AddExpenseFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         args.putInt(USER_ID_KEY, userId);
         fragment.setArguments(args);
         return fragment;
@@ -73,51 +75,26 @@ public class AddExpenseFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
             userId = getArguments().getInt(USER_ID_KEY);
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add_expense, container, false);
         expenseAmountWidget = view.findViewById(R.id.expenseAmountInput);
 
         view.findViewById(R.id.submitNewExpenseButton).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(final View view) {
-                NewPurchasePayload newPurchasePayload = new NewPurchasePayload();
-                newPurchasePayload.setCost(Integer.parseInt(expenseAmountWidget.getText().toString(), 10));
-                ApiServiceSingleton.getInstance().purchaseService
-                        .createPurchase(userId, newPurchasePayload)
-                        .enqueue(new Callback<Purchase>() {
-                            @Override
-                            public void onResponse(Call<Purchase> call, Response<Purchase> response) {
-                                if (mListener != null) {
-                                    mListener.onSubmitExpense(view);
-                                }
-                            }
-
-                            @Override
-                            public void onFailure(Call call, Throwable t) {
-
-                            }
-                        });
-            }
+            public void onClick(final View view) { onClickAddExpense(view); }
         });
 
         return view;
     }
-
-    // TODO: Rename method, update argument and hook method into UI event
-//    public void onButtonPressed(Uri uri) {
-//        if (mListener != null) {
-//            mListener.onFragmentInteraction(uri);
-//        }
-//    }
 
     @Override
     public void onAttach(Context context) {
@@ -136,17 +113,21 @@ public class AddExpenseFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        void onSubmitExpense(final View view);
+    private void onClickAddExpense(final View view) {
+        NewPurchasePayload newPurchasePayload = new NewPurchasePayload();
+        newPurchasePayload.setCost(Integer.parseInt(expenseAmountWidget.getText().toString(), 10));
+        ApiServiceSingleton.getInstance().purchaseService
+                .createPurchase(userId, newPurchasePayload)
+                .enqueue(new Callback<Purchase>() {
+                    @Override
+                    public void onResponse(Call<Purchase> call, Response<Purchase> response) {
+                        if (mListener != null) {
+                            mListener.onSubmitExpense(view);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call call, Throwable t) { }
+                });
     }
 }
