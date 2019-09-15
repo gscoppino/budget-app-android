4cbd59b93020e8b50dcb6d8074b66ccb5198da3a
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2019-09-15 16:45:01 -0400

    Cleaned up RegistrationFragment class implementation.

--- app/src/main/java/com/budgetapp/landing/registration/RegistrationFragment.java
+++ app/src/main/java/com/budgetapp/landing/registration/RegistrationFragment.java
@@ -29,24 +29,30 @@ import retrofit2.Response;
  * create an instance of this fragment.
  */
 public class RegistrationFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
     public static final String USER_USERNAME = "USER_USERNAME";
     public static final String USER_PASSWORD = "USER_PASSWORD";
 
     private EditText monthlySalaryWidget;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     private String userUsername;
     private String userPassword;
 
     private OnFragmentInteractionListener mListener;
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson
+     * <a href="http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     * </p>
+     */
+    public interface OnFragmentInteractionListener {
+        void onLogin(View view, int userId, String userUsername, int monthlySalary);
+    }
 
     public RegistrationFragment() {
         // Required empty public constructor
@@ -56,16 +62,13 @@ public class RegistrationFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param userUsername The user's username.
+     * @param userPassword The user's password.
      * @return A new instance of fragment RegistrationFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static RegistrationFragment newInstance(String param1, String param2, String userUsername, String userPassword) {
+    public static RegistrationFragment newInstance(String userUsername, String userPassword) {
         RegistrationFragment fragment = new RegistrationFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         args.putString(USER_USERNAME, userUsername);
         args.putString(USER_PASSWORD, userPassword);
         fragment.setArguments(args);
@@ -76,16 +79,16 @@ public class RegistrationFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
             userUsername = getArguments().getString(USER_USERNAME);
             userPassword = getArguments().getString(USER_PASSWORD);
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_registration, container, false);
 
@@ -93,36 +96,11 @@ public class RegistrationFragment extends Fragment {
 
         view.findViewById(R.id.appBeginButton).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(final View view) {
-                NewUserPayload newUserPayload = new NewUserPayload();
-                newUserPayload.setUsername(userUsername);
-                newUserPayload.setSalary(Integer.parseInt(monthlySalaryWidget.getText().toString(), 10));
-                ApiServiceSingleton.getInstance().userService.createUser(newUserPayload).enqueue(new Callback<User>() {
-                    @Override
-                    public void onResponse(Call<User> call, Response<User> response) {
-                        if (mListener != null) {
-                            User user = response.body();
-                            mListener.onLogin(view, user.getId(), user.getUsername(), user.getMonthlySalary());
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call call, Throwable t) {
-
-                    }
-                });
-            }
+            public void onClick(View view) { onClickBegin(view); }
         });
 
         return view;
     }
-
-    // TODO: Rename method, update argument and hook method into UI event
-//    public void onButtonPressed(Uri uri) {
-//        if (mListener != null) {
-//            mListener.onBegin(uri);
-//        }
-//    }
 
     @Override
     public void onAttach(Context context) {
@@ -141,17 +119,21 @@ public class RegistrationFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        void onLogin(View view, int userId, String userUsername, int monthlySalary);
+    private void onClickBegin(final View view) {
+        NewUserPayload newUserPayload = new NewUserPayload();
+        newUserPayload.setUsername(userUsername);
+        newUserPayload.setSalary(Integer.parseInt(monthlySalaryWidget.getText().toString(), 10));
+        ApiServiceSingleton.getInstance().userService.createUser(newUserPayload).enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                if (mListener != null) {
+                    User user = response.body();
+                    mListener.onLogin(view, user.getId(), user.getUsername(), user.getMonthlySalary());
+                }
+            }
+
+            @Override
+            public void onFailure(Call call, Throwable t) { }
+        });
     }
 }
